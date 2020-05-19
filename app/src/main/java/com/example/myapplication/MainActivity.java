81655d1d2258eff354691573ec85310916498b18
MrNeverBefore <rohitshaw509@gmail.com>
2020-05-19 11:34:58 +0530

    first commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    public void btnClick(View view){
+        EditText dollarAmount = (EditText) findViewById(R.id.dollarAmount);
+        String dollars = dollarAmount.getText().toString();
+        Double doubleDollar = Double.parseDouble(dollars);
+        Double doubleInr = 71.72* doubleDollar;
+        String toastText = "= "+ doubleInr.toString() + " $";
+        Toast.makeText(this, toastText, Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
